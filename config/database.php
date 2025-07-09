2029101fd71a37931bd9fc28049341fb7843d841
aprilian <apriliandwianggara24@gmail.com>
2025-07-09 17:17:53 +0700

    Directly use individual DB env vars in config

--- config/database.php
+++ config/database.php
@@ -44,7 +44,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
+            
             'host' => env('DB_HOST'),
             'port' => env('DB_PORT'),
             'database' => env('DB_DATABASE'),
